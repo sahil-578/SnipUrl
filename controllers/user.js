915a13a4b4f59c8ac3d998d93510cf06d0cbe3b4
Sahil <sahil1372001@gmail.com>
2024-03-07 17:25:32 +0530

    Final Completion , Use of Authorization Header

--- controllers/user.js
+++ controllers/user.js
@@ -28,8 +28,11 @@ async function handleUserLogin(req, res) {
     const token = setUser(user);
 
     // res.cookie('uid', sessionId);
-    res.cookie('uid', token);
-    return res.redirect('/');
+
+    // res.cookie('uid', token);
+    // return res.redirect('/');
+
+    return res.json({token})
 }
 
 
--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,12 +1,18 @@
 const {getUser} = require('../service/auth');
 
 async function restrictToLoginUserOnly(req, res, next) {
-    const userUuid = req.cookies?.uid;
+    // const userUuid = req.cookies?.uid;
+
+    const userUuid = req.headers['authorization'];
 
     if(!userUuid) 
         return res.redirect('/login');
 
-    const user = getUser(userUuid);
+    const token = userUuid.split('Bearer ')[1];
+
+    // const user = getUser(userUuid);
+
+    const user = getUser(token);
 
     if(!user) 
         return res.redirect('/login');
